655cf647b92ea68c1351951bcc62dd2570334748
zhangwen <71752352@qq.com>
2013-02-17 23:01:46 +0800

    增加了动画和计时器接口，新增了浏览器判断模块，修改了一些bug

--- sjs.js
+++ sjs.js
@@ -1,56 +1,48 @@
 /**
  * build by awen  71752352@qq.com
  */
-(function(window,undefined){
-	var wd=window.document,
-		_$=window.$?window.$:undefined;
-		wn=window.Navigator,
-		wl=window.location,
-		type=function(o){
-			return o!=undefined?Object.prototype.toString.call(o):'undefined';
+;(function(W,undefined){
+	var D=W.document,
+		_$$=W.$?W.$:undefined,
+		wn=W.navigator,
+		wl=W.location,
+		ua=wn.userAgent.toLowerCase(),
+		av=wn.appVersion,
+ 		type=function(o){
+			return o!=undefined?(Object.prototype.toString.call(o)).slice(8,-1):'undefined';
 		},
+		RAF = W.requestAnimationFrame|| W.mozRequestAnimationFrame|| W.webkitRequestAnimationFrame|| W.msRequestAnimationFrame|| W.oRequestAnimationFrame|| function(callback) {setTimeout(callback, _f);},
 		/*基础工具类*/
-		untils={
+		UT={
 			noConflict:function(){
-				if (_$) {
-					window.$=_$;
-				}
+				if (_$$) {W.$=_$$;}
 			},
 			uniqueId:function(){
-				var t=new Date().getTime(),r=parseInt(Math.random()*100000);
-				return t*100000+r;
+				var t=new Date().getTime(),r=parseInt(Math.random()*10000);
+				return t*10000+r;
 			},
+			type:type,
 			isArray:function(o){
-				return type(o)==='[object Array]';
+				return type(o)==='Array';
 			},
 			isBoolean:function(o){
-				return type(o)==='[object Boolean]';
+				return type(o)==='Boolean';
 			},
 			isString:function(o){
-				return type(o)==='[object String]';
+				return type(o)==='String';
 			},
 			isFunction:function(o){
-				return type(o)==='[object Function]';
+				return type(o)==='Function';
 			},
 			isNumeric:function(o){
-				return !isNaN( parseFloat(obj) ) && isFinite( obj );
+				return !isNaN( parseFloat(o) ) && isFinite( o );
 			},
 			isXML: function(el) {
 	            var doc = el.ownerDocument || el;
 	            return doc.createElement("p").nodeName !== doc.createElement("P").nodeName;
 	        },
-			isObject:function(o){
-				return type(o)==='[object Object]';
-			},
 			isPlainObject:function(o){
-				//ie6下 window和dom都是object
-				if (!o||(!this.isObject(o))||o.nodeType||o==window) {
-					return false;
-				}
-				//检查第一个或者最后一个属性为自身属性
-				var k=null;
-				for (k in o) {};
-				return k===undefined||Object.prototype.hasOwnProperty.call(o,k);
+				return type(o)==='Object';
 			},
 			isEmptyObject: function(o) {
 				var name;
@@ -68,8 +60,7 @@
 			},
 			/**核心啊，代码换效率*/
 			each:function(o,f){
-				var isa=sjs.isArray(o);
-				if (isa) {
+				if (sjs.isArray(o)) {
 					for (var i = 0,l=o.length; i < l; i++) {
 						if(f.call(o,i,o[i])===false){
 							break;
@@ -77,7 +68,7 @@
 					}
 				}else{
 					for (var k in o) {
-						if (o.hasOwnProperty(k)) {
+						if (o[k]!=undefined) {
 							if(f.call(o,k,o[k])===false){
 								break;
 							}
@@ -115,9 +106,13 @@
 				}
 				return o;
 			},
-			inArray:function(v,o,i){
-				for (var i=i==undefined?0:i,l=o.length; i < l; i++) {
-					if (o[i]==v) {return i}
+			inArray:function(v,o){
+				if (Array.prototype.indexOf) {
+					  return Array.prototype.indexOf.call(o,v);  
+				}else{
+					for (var i=i==undefined?0:i,l=o.length; i < l; i++) {
+						if (o[i]==v) {return i}
+					}
 				}
 				return -1;
 			},
@@ -160,35 +155,34 @@
 		        		return null;  
 					}
 					d =sjs.trim(d);
-					return window.JSON?JSON.parse(d):eval(d);
+					return W.JSON?JSON.parse(d):eval(d);
 				},
 				stringify:function(O){
-					if (!O) {
-						return '';
+					if (!O) {return '';}
+					if (W.JSON) {return W.JSON.stringify(O);}
+					var S = [],J = "";
+					switch(type(O)){
+						case 'Array':
+							for (var i = 0; i < O.length; i++){
+					       		S.push(this.stringify(O[i])); 
+						    }
+						    J = '[' + S.join(',') + ']'; 
+						break;
+						case 'Date':
+						 	J = "new Date(" + O.getTime() + ")"; 
+						break;
+						case 'RegExp':
+						case 'Function':
+							J = O.toString(); 
+						break;
+						case 'Object':
+						  for (var i in O) { 
+						       O[i] = typeof (O[i]) == 'string' ? '"' + O[i] + '"' : (typeof (O[i]) === 'object' ? this.stringify(O[i]) : O[i]); 
+						       S.push(i + ':' + O[i]); 
+						   } 
+						   J = '{' + S.join(',') + '}'; 
+						break;
 					}
-					if (window.JSON) {
-						return window.JSON.stringify(O);
-					}
-					var S = [],J = ""; 
-					if (type(O) === '[object Array]') { 
-					   for (var i = 0; i < O.length; i++){
-					       S.push(this.stringify(O[i])); 
-					   }
-					   J = '[' + S.join(',') + ']'; 
-					} 
-					else if (type(O) === '[object Date]') { 
-					   J = "new Date(" + O.getTime() + ")"; 
-					} 
-					else if (type(O) === '[object RegExp]' || type(O) === '[object Function]') { 
-					   J = O.toString(); 
-					} 
-					else if (type(O) === '[object Object]') { 
-					   for (var i in O) { 
-					       O[i] = typeof (O[i]) == 'string' ? '"' + O[i] + '"' : (typeof (O[i]) === 'object' ? this.stringify(O[i]) : O[i]); 
-					       S.push(i + ':' + O[i]); 
-					   } 
-					   J = '{' + S.join(',') + '}'; 
-					} 
 					return J; 
 				},
 				parseQuery:function(s){
@@ -222,38 +216,68 @@
 					}
 					return n;
 				}
+			},
+			/**帧动画对象*/
+			raf:function(frame,fn) {
+				//run 动画状态  0初始 1运动 2暂停 3停止
+				var st=new Date().getTime(),_f=Math.ceil(1000/frame),run=0,
+					cnt=0,args=Array.prototype.slice.call(arguments,2);args.unshift(cnt);
+				instance=this;
+	 			function go(){
+	 				if (run<2) {
+	 					var _t=new Date().getTime();
+						var _d=_t-st;
+						if (_d>=_f) {
+							st=_t;
+							cnt++;
+							args[0]=cnt;
+							if(fn.apply(instance,args)===false){
+								return;
+							};
+						};
+						RAF(go);
+	 				};
+				}
+				this.sign=null;
+				this.start=function(){
+					if (run==0) {
+						run=1;
+						RAF(go);
+					};
+				}
+				this.pause=function(){
+					if (run==1) {
+						run=2;
+					};
+				}
+				this.resume=function(){
+					if (run==2) {
+						run=1;
+						RAF(go);
+					};
+				}
+	 			this.stop=function(){
+	 				run=3;
+				}
+				return this;
 			}
 		},
 		//构建sjs对象
-		sjs=function(s){return sjs.fn._init(s);};
+		sjs=function(s){return new sjs.fn.init(s);};
 		//对象原型
 		sjs.fn=sjs.prototype={
+			constructor: sjs,
 			selector: "",
 			length: 0,
-			get:function(i){
-				return this[i];
-			},
-			index:function(){
-				var d=this[0],p=d.parentNode,cs=p.childNodes,idx=0;
-				for (var i= 0,l=cs.length;i<l;i++) {
-					if(cs[i].nodeType==1){
-						if (cs[i]==d) {
-							return idx;
-						}
-						idx++;
-					}
-				}
-				return -1;
-			},
-			_init:function(s){
-				if (window==this) {return new sjs(s)};
+			init:function(s){
+				if (W==this) {return new sjs(s)};
 				if (!s) {
 					return this;
 				}
 				/**如果是函数则为ready*/
 				if (sjs.isFunction(s)) {
 					document.addEventListener("DOMContentLoaded", function(e) {
-						s.call(window,sjs);
+						s.call(W,sjs);
 					});
 				}
 				// sjs(DOMElement)
@@ -261,25 +285,34 @@
 					this[0] = s;
 					this.length = 1;
 					//dom对象的唯一标志
-					index(s);
+					id(s);
 				}
 				// sjs(string)
 				if ( typeof s === "string" ) {
 					if ( s.charAt(0) === "<" && s.charAt( s.length - 1 ) === ">" && s.length >= 3 ) {
 						//暂时不处理html代码
 					} else {
-						var doms=wd.querySelectorAll(s);
+						var doms=D.querySelectorAll(s);
 						this.length = doms.length;
 						for (var i = 0,len=doms.length; i<len; i++) {
-							index(doms[i]);
+							id(doms[i]);
 							this[i]=doms[i];
 						}
 						this.selector=s;
 					}
 				}
 				return this;
+			},
+			get:function(i){
+				var gs=Array.prototype.slice.call(this,0,this.length);
+				return i==undefined?gs:gs[i];
+			},
+			index:function(){
+				var d=this[0],cs=d.parentNode.children;
+				return sjs.inArray(d,cs);
 			}
 		}
+		sjs.fn.init.prototype = sjs.fn;
 		//主要扩展方法
 		sjs.extend=sjs.fn.extend=function(){
 			var to=arguments[0]||{},options,len=arguments.length,deep=false,ci=0,src,copy;
@@ -308,12 +341,12 @@
 				 			continue;
 				 		}
 				 		//深度复制
-				 		if (deep&&copy&&(untils.isArray(copy)||untils.isPlainObject(copy))) {
-				 			if (untils.isPlainObject(copy)) {
-				 				src= src && untils.isArray(src) ? src : {};
+				 		if (deep&&copy&&(UT.isArray(copy)||UT.isPlainObject(copy))) {
+				 			if (UT.isPlainObject(copy)) {
+				 				src= src && UT.isArray(src) ? src : {};
 				 			}
-				 			if (untils.isArray(copy)) {
-				 				src= src && untils.isPlainObject(src) ? src : [];
+				 			if (UT.isArray(copy)) {
+				 				src= src && UT.isPlainObject(src) ? src : [];
 				 			}
 				 			to[key] = sjs.extend( deep, src, copy );
 				 		}else{
@@ -326,12 +359,18 @@
 		}
 		///////////////////////////////////sjs对象扩展
 		//获取dom对象的唯一标志
-		function index(d){
-			return d._hash?d._hash:(d._hash=untils.uniqueId());
+		function id(d){
+			return d._hash?d._hash:(d._hash=UT.uniqueId());
+		}
+		//将CSS属性名转换成驼峰式
+		function camelize(s) {
+		 return s.replace(/-[a-z]/gi,function (c) {
+		  return c.charAt(1).toUpperCase();
+		 });
 		}
 		//获取class值
 		function gc(d,n){
-			return window.getComputedStyle(d).getPropertyValue(n)||'';
+			return W.getComputedStyle(d).getPropertyValue(n)||'';
 		}
 		//检测是否含有className
 		function hc(d,c){
@@ -351,18 +390,18 @@
 		}
 		//获取元素数据
 		function gd(d,k){
-		 	var hash=index(d),ds=_domdatas[hash]||undefined;
+		 	var hash=id(d),ds=_domdatas[hash]||undefined;
 		 	return k==undefined?ds:(ds&&ds[k])?ds[k]:undefined;
 		}
 		//设置元素数据
 		function sd(d,k,v){
-			var hash=index(d);
+			var hash=id(d);
 			if (!_domdatas[hash]) {_domdatas[hash]={};}
 			_domdatas[hash][k]=v;
 		}
 		//删除数据
 		function dd(d,k){
-			var hash=index(d),ds=_domdatas[hash]||undefined;
+			var hash=id(d),ds=_domdatas[hash]||undefined;
 			if (ds){
 				if(k){
 					if (_domdatas[hash][k]) {
@@ -422,6 +461,7 @@
 				}
 			}
 		}
+		//dom扩展
 		var _domdatas={},DOMS={
 	 		data:function(k,v){
 	 			if (this.length>0) {
@@ -472,7 +512,7 @@
 	 			});
 	 		},
 	 		val:function(s){
-	 			return s==undefined?this[0].value:this.each(function(d,i){
+	 			return s==undefined?(this[0].value||''):this.each(function(d,i){
 	 				if (sjs.isFunction(s)) {
 	 					var r=s.call(d,i,d.value);
 	 					if (r!=undefined) {
@@ -504,16 +544,19 @@
 	 			}
 	 		},
 	 		css:function(a,b){
-	 			if (a==undefined||this.length==0) {return this};
+	 			if (a==undefined||this.length==0) {return undefined};
 	 			if (sjs.isString(a)) {
-	 				var isf=sjs.isFunction(b);
+	 				var isf=sjs.isFunction(b),a=camelize(a);
 	 				return b==undefined?gc(this[0],a):this.each(function(d,i){
-	 					d.style[a]=isf?(b.call(this,i,gc(this,a))):b;
+	 					var v=isf?(b.call(this,i,gc(this,a))):b;
+	 					v=sjs.isNumeric(v)?(v+'px'):v;
+	 					d.style[a]=v;
 	 				});
 	 			}
 	 			if (sjs.isPlainObject(a)) {
 	 				var ins=this;
 	 				sjs.each(a,function(k,v){
+	 					k=camelize(k);
 	 					ins.css(k,v);
 	 				});
 	 			}
@@ -558,8 +601,8 @@
 	 		},
 	 		getBox:function(){
 				//safari3.2没有getBoundingClientRect
-				if (window.HTMLElement&&(!('getBoundingClientRect' in HTMLElement))) {
-					var st = wd.documentElement.scrollTop,sl = wd.documentElement.scrollLeft,al = this[0].offsetLeft,
+				if (W.HTMLElement&&(!('getBoundingClientRect' in HTMLElement))) {
+					var st = D.documentElement.scrollTop,sl = D.documentElement.scrollLeft,al = this[0].offsetLeft,
 						at = this[0].offsetTop,
 						cp = this[0].offsetParent; 
 					while (cp != null){   
@@ -668,8 +711,16 @@
 						});
 					}
 				});
-			}
+			},
+			//筛选
+			eq:function(i){
+				i=i<0?this.length+i:i;
+				return sjs(this.get(i));
+			},
+			//文档处理
+
 	 	},
+	 	//ajax
 	 	AJAXS={
 	 		ajax:function(url,s){
 				var _s = {
@@ -765,9 +816,48 @@
 				}
 				return sjs.ajax(u,{type:'post',success:_f,data:_d,dataType:_t});
 			}
-	 	};
+	 	},
+	 	/**浏览器扩展*/
+	 	gwe=function(p){
+			try {
+	            if (window.external && window.external[p]) {
+	                var f=W.external[p];
+	                return UT.isFunction(f)?f():f;
+	            }
+			} catch (e) {}
+            return '';
+        },
+		bs={
+			isAndroid  		:(/Android/i).test(ua),
+   			isIPad  		:(/ipad/i).test(ua),
+   			isIPhone  		:(/iphone os/i).test(ua),
+   			isWMobile		:(/Ws mobile/i).test(ua),
+
+			isIECore		:(/Trident/i).test(ua),
+			isWebkitCore	:(/webkit/i).test(ua),
+			isGeckosCore	:(/Gecko/i).test(ua) && !(/khtml/i).test(ua),
+			se360			:(function(){
+						        var ret =/360se/i.test(ua)|| /360ee/i.test(ua);
+						        return ret?ret:(/360se/i).test(gwe('twGetRunPath'));
+							})(),
+			sougou			:(/MetaSr/i).test(ua),
+			qq				:(/QQBrowser/i).test(ua),
+			maxthon			:(function(){
+								return gwe('max_version').substr(0,1)>0;
+					 		})(),
+			opera			:W.opera?true:false,
+			firefox 		:(/Firefox/i).test(ua),
+			uc  			:(/UC/i).test(ua),
+			liebao			:(/LBBROWSER/i).test(ua),
+			baidu			:(/BIDUBrowser/i).test(ua)||gwe('GetVersion')=='baidubrowser'
+		},
+		nogc=!bs.sougou&&!bs.maxthon&&!bs.qq&&!bs.uc&&!bs.liebao&&!bs.baidu&&!bs.se360;
+		bs.ie=(bs.isIECore&&nogc);
+		bs.chrome=( /Chrome/i).test(ua)&&W.chrome&&nogc;
+		bs.safari=(/Safari/.test(ua))&&!bs.chrome&&nogc;
+		bs.prefix=bs.isWebkitCore?'webkit':bs.isGeckosCore?'Moz':bs.opera?'O':bs.isIECore?'ms':'';
 	 	/**事件管理栈*/
-	 	var _listeners={},
+	 	var _ES={},
 		EVENTS={
 			on:function(){
 				if (arguments.length>1){
@@ -779,35 +869,34 @@
 							return this;
 						}
 					}
-					if (typeof _listeners[et] === "undefined") {	
-						_listeners[et]={};
+					if (typeof _ES[et] === "undefined") {	
+						_ES[et]={};
 						//事件委托
 						document.addEventListener(et,function(e){
-							if(_listeners[e.type]){
+							if(_ES[e.type]){
 								sjs(e.target).trigger(e.type,e);
 							}
 						},false);
 					}
 					//将本次选择器放入本对象的data中，便于以后循环获取根据不同selector绑定的事件
 					this.each(function(dom){
-						var hash=index(dom);
-						if (typeof _listeners[et][hash] === "undefined") {
-							_listeners[et][hash]=[];
+						var hash=id(dom);
+						if (typeof _ES[et][hash] === "undefined") {
+							_ES[et][hash]=[];
 						}
-						_listeners[et][hash].push({'fn':fn,'data':d,'cnt':0});
+						_ES[et][hash].push({'fn':fn,'data':d,'cnt':0});
 					});
 				}
 				return this;
 			},
 			trigger:function(et){
-				if (et && _listeners[et]) {
-					var _dels=[];
+				if (et && _ES[et]) {
+					var _dels=[],e=arguments[arguments.length-1];;
 					//在本对象的事件选择器列表中获取所有绑定事件时使用过的选择器来检索事件
 					this.each(function(d){
 						//始终传入的最后一个参数为event(手动除外)
-						var e=arguments[arguments.length-1];
 						e=e.type?e:{'type':et,'target':d};
-						var hash=index(d),items=_listeners[et][hash]||[],_dels=[];
+						var hash=id(d),items=_ES[et][hash]||[],_dels=[];
 						for (var j=0,len = items.length; j <len; j++) {
 							e.data=items[j]['data'];
 							e.firecnt=++items[j]['cnt'];
@@ -831,9 +920,9 @@
 			},
 			off:function(et,fn){
 					this.each(function(d){
-						var hash=index(d),items;
-						if (et && _listeners[et]) {
-							if (items=_listeners[et][hash]) {
+						var hash=id(d),items;
+						if (et && _ES[et]) {
+							if (items=_ES[et][hash]) {
 								if(fn){
 									var _dels=[];
 									for (var i =items.length - 1; i >= 0; i--) {
@@ -843,15 +932,15 @@
 									}
 									//数组分离删除，防止序号错乱
 									for (var i = _dels.length - 1; i >= 0; i--) {
-										_listeners[et][hash].splice(i,1);
+										_ES[et][hash].splice(i,1);
 									}
 								}else{
-									delete _listeners[et][hash];
+									delete _ES[et][hash];
 								}
 							}
 						}else{
 							//删除所有
-							sjs.each(_listeners,function(i,d){
+							sjs.each(_ES,function(i,d){
 								delete d[hash];
 							});
 						}
@@ -868,17 +957,126 @@
 					return false;
 				});
 				return this;
-			},
-			bind:this.on,
-			unbind:this.off
+			}
 		};
+		EVENTS.bind=EVENTS.on;EVENTS.unbind=EVENTS.off;
+		/**动画*/
+		var atimer=null,_AS={},_apre=bs.prefix?bs.prefix+'Transition':'transition',ntdelay=_apre+'Delay',ntp=_apre+'Property',ntd=_apre+'Duration',nttf=_apre+'TimingFunction',
+		speed={'slow':1000,'normal':600,'fast':300},
+		gfx=function(d){
+			var i=id(d),q=_AS[i];
+			if (q==undefined) {
+				var ds=d.style,ot={};
+				ot[ntp]=ds[ntp];ot[ntd]=ds[ntd];ot[nttf]=ds[nttf];ot[ntdelay]=ds[ntdelay];
+				return {dom:d,oldt:ot,t:0,stacks:[]};
+			}
+			return  _AS[i];
+		},
+		ANIMS={
+			delay:function(t){
+				if (sjs.isNumeric(t)) {
+					$(this).each(function(d){
+						var q=gfx(d);
+							q.stacks.push({'dur':t});
+						_AS[id(d)]=q;
+					});
+				}
+				this._run();
+				return this;
+			},
+			/**stop([clearQueue])
+			停止所有在指定元素上正在运行的动画。
+			如果队列中有等待执行的动画(并且clearQueue没有设为true)，他们将被马上执行
+			*/
+			stop:function(cq){
+				cq=cq===false?false:true;
+				this.each(function(d){
+					if (cq) {
+						//中断
+						var s=d.style;
+						for (var i = s.length - 1; i >= 0; i--) {
+							d.style[s[i]]=gc(d,s[i]);
+						}
+					}else{
+						var as=_AS[id(d)],l=as?as.stacks.length:0,p={};
+						for (var i = 0; i <l; i++) {
+							if (as.stacks[i].tran) {
+								sjs.extend(p,as.stacks[i].tran)
+							}
+						}
+						p[ntp]='none';
+						sjs(d).css(p);
+					}
+					delete _AS[id(d)];
+				});
+				return this;
+			},
+			/**animate(params,[speed],[easing],[fn])*/
+			animate:function(p,s,e,f){
+				if (!sjs.isPlainObject(p)||sjs.isEmptyObject(p)) {return this;};
+				var	_s=(s==undefined||sjs.isPlainObject(s))?100:s,
+					_e=!e?'ease':e,
+					_f=f;
+					if (sjs.isPlainObject(s)) {
+						_s=s.speed?s.speed:_s;
+						_e=s.easing?s.easing:_e;
+						_f=s.callback?s.callback:_f;
+					};
+					_s=sjs.isString(_s)?(speed[_s]||100):_s;
+					_e=_e=='swing'?'ease-in-out':_e;
+				//更新动画队列
+ 				this.each(function(d){
+					var queue=gfx(d),tran={};
+					tran[ntp]='all';tran[ntd]=_s/1000+'s';tran[nttf]=_e;
+					sjs.extend(tran,p);
+					queue.stacks.push({dur:_s,tran:tran,fn:_f});
+					_AS[id(d)]=queue;
+				});
+				this._run();
+				return this;
+			},
+			/***/
+			_run:function(){
+				//启动全局动画队列
+				if (atimer==null) {
+					atimer=UT.raf(25,function(n){
+						var n=0;
+						sjs.each(_AS,function(i,q){
+							if (q.stacks.length==0) {
+								//本对象的动画序列为空，还原并删除队列中的该对象
+								sjs(q.dom).css(q.oldt);
+								delete _AS[i]
+							}else{
+								var stack=q.stacks[0];
+								//本dom的计数器开始
+								if (q.t==0) {
+									q.t=Date.now();
+									if (stack.tran!=undefined) {
+										sjs(q.dom).css(stack.tran);
+									}
+								}else{
+									//判断结束
+									if (Date.now()-q.t>=stack.dur){
+										if(stack.fn){stack.fn.call(q.dom);}
+										q.t=0;
+										_AS[i].stacks.shift();
+									}
+								}
+							}
+							n++;
+						});
+						if (n==0) {this.stop();atimer=null};
+					});
+					atimer.start();
+				};
+			}
+		},
 		/**
-		 * 扩展常用工具包
+		 * 扩展所有工具包
 		 */
-		sjs.extend(untils);
-		sjs.extend(AJAXS);
-		sjs.fn.extend(DOMS);
-		sjs.fn.extend(EVENTS);
+		SED=sjs.extend({},UT,{browser:bs},AJAXS),ED=sjs.extend(DOMS,EVENTS,ANIMS);
+		sjs.extend(SED);
+		sjs.fn.extend(ED);
 		//全局提供
-		window.sjs = window.$ = sjs;
+		W.sjs = W.$ = sjs;
 })(window);
